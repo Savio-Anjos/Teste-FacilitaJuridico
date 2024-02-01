96201be22b3d6e4673dd2909a7716c8cbd023a5c
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-01 00:24:41 -0300

    test :white_check_mark: Added unitary tests for list all customers use case

--- back-end/src/use-cases/list-all-customers.spec.ts
+++ back-end/src/use-cases/list-all-customers.spec.ts
@@ -1,27 +1,25 @@
 import { CustomersRepository } from "@/repositories/customers-repository";
 import { beforeEach, describe, expect, it } from "vitest";
 import { InMemoryCustomersRepository } from "@/repositories/in-memory/in-memory-customers-repository";
-import { FilterCustomersByTelephoneUseCase } from "./filter-customers-by-telephone";
+import { ListAllCustomersUseCase } from "./list-all-customers";
 
 let customersRepository: CustomersRepository;
-let sut: FilterCustomersByTelephoneUseCase;
+let sut: ListAllCustomersUseCase;
 
-describe("Filter Customers By Telephone  Use Case", () => {
+describe("List All Customers Use Case", () => {
   beforeEach(() => {
     customersRepository = new InMemoryCustomersRepository();
-    sut = new FilterCustomersByTelephoneUseCase(customersRepository);
+    sut = new ListAllCustomersUseCase(customersRepository);
   });
 
-  it("should be able to filter customers by telephone", async () => {
+  it("should be able to list all customers", async () => {
     await customersRepository.create({
       name: "John Doe",
       email: "johndoe@example.com",
       telephone: "99 99999-9999",
     });
 
-    const { customers } = await sut.execute({
-      telephone: "99 99999-9999",
-    });
+    const { customers } = await sut.execute();
 
     expect(customers).toHaveLength(1);
   });
