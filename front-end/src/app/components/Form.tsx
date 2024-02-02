d1f8d282be5e30bde3fd071752dca94154986f27
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-02 17:04:24 -0300

    feat: :sparkles: Updated form component

--- front-end/src/app/components/Form.tsx
+++ front-end/src/app/components/Form.tsx
@@ -5,14 +5,28 @@ const Form = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [telephone, setTelephone] = useState("");
+  const [clientLatitude, setClientLatitude] = useState("")
+  const [clientLongitude, setClientLongitude] = useState("")
+  const [companyLatitude, setCompanyLatitude] = useState("")
+  const [companyLongitude, setCompanyLongitude] = useState("")
 
   async function createClient(event: FormEvent) {
     event.preventDefault(); 
 
+    const clientLatitudeNumber = Number(clientLatitude)
+    const clientLongitudeNumber = Number(clientLongitude)
+    const companyLatitudeNumber = Number(companyLatitude)
+    const companyLongitudeNumber = Number(companyLongitude)
+
     const client = {
       name,
       email,
-      telephone
+      telephone,
+      clientLatitude: clientLatitudeNumber,
+      clientLongitude: clientLongitudeNumber,
+      companyLatitude: companyLatitudeNumber,
+      companyLongitude: companyLongitudeNumber
+    
     };
 
      await fetch("http://localhost:3333/customers", {
@@ -26,6 +40,10 @@ const Form = () => {
     setName("")
     setEmail("")
     setTelephone("")
+    setClientLatitude("")
+    setClientLongitude("")
+    setCompanyLatitude("")
+    setCompanyLongitude("")
 
 
   }
@@ -63,6 +81,50 @@ const Form = () => {
             placeholder="73 998536538"
             value={telephone}
             onChange={(event) => setTelephone(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="clientLatitude">Digite a latitude do cliente:</label>
+          <input
+            type="text"
+            id="clientLatitude"
+            placeholder="-13.8399225"
+            value={clientLatitude}
+            onChange={(event) => setClientLatitude(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="clientLongitude">Digite a longitude do cliente:</label>
+          <input
+            type="text"
+            id="clientLongitude"
+            placeholder="-13.8399225"
+            value={clientLongitude}
+            onChange={(event) => setClientLongitude(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="companyLatitude">Digite a latitude da empresa:</label>
+          <input
+            type="text"
+            id="companyLatitude"
+            placeholder="-13.8399225"
+            value={companyLatitude}
+            onChange={(event) => setCompanyLatitude(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="companyLongitude">Digite a longitude da empresa:</label>
+          <input
+            type="text"
+            id="companyLongitude"
+            placeholder="-13.8399225"
+            value={companyLongitude}
+            onChange={(event) => setCompanyLongitude(event.target.value)}
           />
         </div>
 
