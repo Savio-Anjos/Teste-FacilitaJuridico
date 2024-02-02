9c3f56d80b2efad007257587c626e39be46f6bb6
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-02 03:52:57 -0300

    fix: :bug: Error in paths fixed

--- back-end/vite.config.ts
+++ back-end/vite.config.ts
@@ -3,6 +3,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@/": `${process.cwd()}/src/`,
+    },
+  },
   test: {
     environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
   },
