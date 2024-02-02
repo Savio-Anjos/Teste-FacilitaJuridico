2038a4fa3bcea6c35b8c746a86593d73dcad9ad0
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-02 11:16:12 -0300

    feat: :sparkles: Added dynamic customer listing

--- front-end/src/app/components/Table.tsx
+++ front-end/src/app/components/Table.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import styles from "./Table.module.css";
 
-const Table = () => {
+import { Customer } from '../page';
+
+interface TableProps {
+  customers: Customer[];
+}
+
+const Table: React.FC<TableProps> = ({ customers }) => {
   return (
     <section className={styles.containerTable}>
     <table className={styles.table}>
@@ -10,16 +16,16 @@ const Table = () => {
  <th>Email</th>
  <th>Telefone</th>
 </tr>
-<tr>
- <td>João Silva</td>
- <td>joao@example.com</td>
- <td>(11) 98765-4321</td>
-</tr>
-<tr>
- <td>Maria Souza</td>
- <td>maria@example.com</td>
- <td>(22) 12345-6789</td>
-</tr>
+{customers.map((client) => {
+  return (
+    <tr key={client.id}>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.telephone}</td>
+    </tr>
+  )
+})}
+
 
 </table>
     </section>
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,33 +1,57 @@
-"use client"
-
+"use client";
 import styles from "./page.module.css";
 import Table from "./components/Table";
 import Form from "./components/Form";
 import Filter from "./components/Filter";
 import { useState } from "react";
 
+export interface Customer {
+  id: string
+  name: string;
+  email: string;
+  telephone: string;
+}
+
 export default function Home() {
+  const [isCreating, setIsCreating] = useState(false);
+  const [isListing, setIsListing] = useState(false);
+  const [isFiltering, setIsFiltering] = useState(false);
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null>(null);
 
-  const [isCreating, setIsCriating] = useState(true)
-  const [isListing, setIsListing] = useState(false)
-  const [isFiltering, setIsFiltering] = useState(false)
+  async function createClient() {
+    setIsCreating(true);
+    setIsListing(false);
+    setIsFiltering(false);
 
-  function createClient() {
-    setIsCriating(true)
-    setIsListing(false)
-    setIsFiltering(false)
   }
 
-  function listCustomers() {
-    setIsListing(true)
-    setIsCriating(false)
-    setIsFiltering(false)
+  
+
+  async function listCustomers() {
+    setIsListing(true);
+    setIsCreating(false);
+    setIsFiltering(false);
+
+    
+      const response = await fetch("http://localhost:3333/customers")
+
+      const data = await response.json();
+
+      setCustomers(data.customers as Customer[]);
+
+
+      console.log(customers)
+    
+
+   
   }
 
-  function filterCustomers() {
-    setIsFiltering(true)
-    setIsListing(false)
-    setIsCriating(false)
+  async function filterCustomers() {
+    setIsFiltering(true);
+    setIsListing(false);
+    setIsCreating(false);
   }
 
 
@@ -35,22 +59,34 @@ export default function Home() {
     <main className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>
-           Gerenciamento de Clientes
+          Gerenciamento de Clientes
         </h1>
 
-       <section className={styles.containerButtons}>
+        <section className={styles.containerButtons}>
+          <button
+            className={styles.button}
+            onClick={createClient}
+          >
+            Cadastrar Cliente
+          </button>
+          <button
+            className={styles.button}
+            onClick={listCustomers}
+          >
+            Listar Clientes
+          </button>
+          <button
+            className={styles.button}
+            onClick={filterCustomers}
+          >
+            Filtrar Clientes
+          </button>
+        </section>
 
-        <button className={styles.button} onClick={createClient}>Cadastrar Cliente</button>
-        <button className={styles.button} onClick={listCustomers}>Listar Clientes</button>
-        <button className={styles.button} onClick={filterCustomers}>Filtrar Clientes</button>
-       </section>
-     
-         {isCreating && <Form />  } 
-         {isListing && <Table />  }
-         {isFiltering && <Filter />  }
-
-
-     </div>
+        {isCreating && <Form />}
+        {isListing && <Table customers={customers} />}
+        {isFiltering && <Filter />}
+      </div>
     </main>
   );
 }
