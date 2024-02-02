f9c8032355c5c6aa0708196d401dfd39e82664fe
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-02 05:01:59 -0300

    feat: :sparkles: Added filter customers by telephone controller

--- back-end/src/http/controllers/routes.ts
+++ back-end/src/http/controllers/routes.ts
@@ -3,10 +3,12 @@ import { createClient } from "./create-client";
 import { listAllCustomers } from "./list-all-customers";
 import { filterCustomersByName } from "./filter-customers-by-name";
 import { filterCustomersByEmail } from "./filter-customers-by-email";
+import { filterCustomersByTelephone } from "./filter-customers-by-telephone";
 
 export async function clientRoutes(app: FastifyInstance) {
   app.post("/customers", createClient);
   app.get("/customers", listAllCustomers);
   app.get("/customers/filter/name/:name", filterCustomersByName);
   app.get("/customers/filter/email/:email", filterCustomersByEmail);
+  app.get("/customers/filter/telephone/:telephone", filterCustomersByTelephone);
 }
