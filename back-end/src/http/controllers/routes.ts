b0fa9d1307f3470e417ad4fe1c9086df53458e44
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-02 16:27:31 -0300

    feat: :sparkles: Added list all customers by distance controller

--- back-end/src/http/controllers/routes.ts
+++ back-end/src/http/controllers/routes.ts
@@ -4,6 +4,7 @@ import { listAllCustomers } from "./list-all-customers";
 import { filterCustomersByName } from "./filter-customers-by-name";
 import { filterCustomersByEmail } from "./filter-customers-by-email";
 import { filterCustomersByTelephone } from "./filter-customers-by-telephone";
+import { listAllCustomersByDistance } from "./list-all-customers-by-distance";
 
 export async function clientRoutes(app: FastifyInstance) {
   app.post("/customers", createClient);
@@ -11,4 +12,5 @@ export async function clientRoutes(app: FastifyInstance) {
   app.get("/customers/filter/name/:name", filterCustomersByName);
   app.get("/customers/filter/email/:email", filterCustomersByEmail);
   app.get("/customers/filter/telephone/:telephone", filterCustomersByTelephone);
+  app.get("/customers/distance", listAllCustomersByDistance);
 }
